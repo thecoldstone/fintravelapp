d6bdc3abe2c46aa8474cc1051f6fe66659976a9f
Nikita Zhukov <nikita.zhukov@sap.com>
2023-05-18 13:15:16 +0200

    Remove rent page & remove navigation items

--- components/layout.tsx
+++ components/layout.tsx
@@ -15,22 +15,6 @@ export default function Layout({ children }) {
         <div className="">
             <div className="relative min-h-full z-10 bg-white lg:max-w lg:w-full xl:max-w">
                 <Navigation />
-                {
-                    maintenanceShown
-                        ? (
-                            <div className="flex py-4 bg-blue-100 dark:bg-blue-200 justify-center">
-                                <div className="flex items-center text-sm font-medium text-blue-700">
-                                    <RiToolsFill size="2em" color="blue" />
-                                    <p>{t("UNDERCONSTRUCTION")}</p>
-                                </div>
-                                <button type="button" className="ml-1 pt-1.5 opacity-30 hover:opacity-100 bg-blue-500 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex h-8 w-8 dark:bg-blue-200 dark:text-blue-600 dark:hover:bg-blue-300" onClick={(e) => { setMaintenanceShown(false) }}>
-                                    <span className="sr-only">Close</span>
-                                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                </button>
-                            </div>
-                        )
-                        : null
-                }
                 <main>{children}</main>
             </div>
             <footer className="bottom-0 w-full bg-gray-50 mt-10 py-10 z-20 font-light text-gray-400">
--- components/navigation/const.ts
+++ components/navigation/const.ts
@@ -1,10 +1,4 @@
 const navigation = [
-    { text: "HOME", url: "/" },
-    {
-        text: "SCHEDULE", navigation: [
-            { text: "RENTBUS", url: "rent" }
-        ]
-    }
 ];
 
 export default navigation;
